e7561fd5961bb6f66e4237974db1c773b3d3140f
10thlds <108203094+10thlds@users.noreply.github.com>
2022-10-19 18:48:02 -0500

    Update 9.PalindromeNum.cpp

--- Easy/9.PalindromeNum.cpp
+++ Easy/9.PalindromeNum.cpp
@@ -3,7 +3,7 @@ Given an integer x, return true if x is palindrome integer.
 
 An integer is a palindrome when it reads the same backward as forward.
 
-For example, 121 is a palindrome while 123 or -121 are not.
+For example, 121 is a palindrome while 123 and -121 are not.
 */
 
 class Solution {
