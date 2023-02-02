adbab1aaac6cea1173782783c64b8a125d82b587
10thlds <108203094+10thlds@users.noreply.github.com>
2023-02-01 18:01:17 -0600

    Update 13.RomanToInteger.cpp

--- Easy/13.RomanToInteger.cpp
+++ Easy/13.RomanToInteger.cpp
@@ -4,7 +4,7 @@
 class Solution {
 public:
 	int romanToInt(std::string s)
-    {
+	{
         std::unordered_map<char, int> rToI = {{'M', 1000}, {'D', 500}, {'C', 100}, {'L', 50}, {'X', 10}, {'V', 5}, {'I', 1}};
         int res = 0;
         for (size_t i=0; i<s.size(); i++)
