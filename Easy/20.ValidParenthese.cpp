efd59384d814b9b78f65061e6a538313ae250d89
10thlds <108203094+10thlds@users.noreply.github.com>
2023-02-07 20:48:14 -0600

    Create 20.ValidParenthese.cpp

---
+++ Easy/20.ValidParenthese.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isValid(std::string s)
+    {        
+        std::stack<char> stk;
+        for (const auto& c:s)
+        {
+            switch(c)
+            {
+                case '(': stk.push(')'); break;
+                case '{': stk.push('}'); break;
+                case '[': stk.push(']'); break; // only close paren is inserted
+
+                default:
+                    if(stk.empty()||c!=stk.top()) return false;
+                    else stk.pop(); // close paren is removed, stack is empty
+            }
+        }
+        return stk.empty();
+    }
+};
